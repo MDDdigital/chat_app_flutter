cc7b79fced43b92e8784aeb72ba07009b71cfe00
MDDdigital <momodiarradia01@gmail.com>
2022-10-11 15:24:43 +0000

    first commit

---
+++ chat_app_flutter/lib/shared/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:528799295761:web:e30fb87af0bac16a6e04b0";
+  static String apiKey = "AIzaSyCRIv02HCe4qF1KAG0YLK2mVLn-4z8m0EQ";
+  static String messagingSenderId = "528799295761";
+  static String projectId = "chatappflutter-5620e";
+  static String authDomain = "chatappflutter-5620e.firebaseapp.com";
+  final primaryColor = const Color(0xFFee7b64);
+}
